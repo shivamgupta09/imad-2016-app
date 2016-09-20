ba394d81a35c0079f559a0a1cc76c7f046bcefea
shivamgupta09 <shivam.gupta09@gmail.com>
2016-09-20 23:32:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,15 +9,15 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/action-one', function (req, res) {
+app.get('/article-one', function (req, res) {
   res.sendFile("The First request will be handeled here");
 });
 
-app.get('/actionptwo', function (req, res) {
+app.get('/article-two', function (req, res) {
   res.sendFile("The Second request will be handeled here");
 });
 
-app.get('/action-three', function (req, res) {
+app.get('/article-three', function (req, res) {
   res.sendFile("The Third request will be handeled here");
 });
 
