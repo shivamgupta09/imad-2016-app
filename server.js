c323317581f05cde845ec945af57f427f816ee9b
shivamgupta09 <shivam.gupta09@gmail.com>
2016-09-28 23:28:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -96,7 +96,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/:articleNames', function (req, res) {
+app.get('/:articleName', function (req, res) {
     var articleName= req.params.articleName;
   res.send(createTemplate(Articles[articleNames]));
 });
