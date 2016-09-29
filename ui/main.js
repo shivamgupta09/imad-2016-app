098917875c34513adb056c2559158c692d1ae2a7
shivamgupta09 <shivam.gupta09@gmail.com>
2016-09-29 17:44:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,5 +3,5 @@ console.log('Loaded!');
 var element=document.getElementById("image");
 element.onclick = function()
 {
-    element.style.marginLeft = "100px";
+    elemeknt.style.marginLeft = "100px";
 }
