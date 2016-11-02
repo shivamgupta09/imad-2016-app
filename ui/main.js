061bda1b8477bc23661f0402321b8665a1f5636c
shivamgupta09 <shivam.gupta09@gmail.com>
2016-11-03 01:39:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,18 @@
+/*console.log('Loaded!');
+
+var element=document.getElementById("image");
+var marginLeft =0;
+function moveRight()
+{
+     marginLeft = marginLeft +1;
+    image.style.marginLeft = marginLeft +"px";
+}
+element.onclick = function()
+{
+    var interval = setInterval(moveRight,50);
+    //element.style.marginLeft = "100px";
+}*/
+
 var button = document.getElementById('counter');
 
 button.onclick = function() {
