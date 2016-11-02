a898472edbf1ff608d2ee4f311ecb92f7b62e16d
shivamgupta09 <shivam.gupta09@gmail.com>
2016-11-03 01:19:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -33,9 +33,8 @@ button.onclick = function () {
         
     };
         
-
+request.open('GET','http://shivamgupta09.imad.hasura-app.io/counter',true);
+request.send(null);
     
 };
 
-request.open('GET','http://shivamgupta09.imad.hasura-app.io/counter',true);
-request.send(null);
