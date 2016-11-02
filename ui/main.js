85888b675c3ad722012f6fd5410264c7e8bdb578
shivamgupta09 <shivam.gupta09@gmail.com>
2016-11-03 00:46:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,4 @@
-console.log('Loaded!');
+/*console.log('Loaded!');
 
 var element=document.getElementById("image");
 var marginLeft =0;
@@ -11,8 +11,8 @@ element.onclick = function()
 {
     var interval = setInterval(moveRight,50);
     //element.style.marginLeft = "100px";
-}
-/*
+}*/
+
 var counter= 0;
 
 var button = document.getElementById("counter");
@@ -42,4 +42,4 @@ button.onclick = function () {
 };
 
 request.open("GET","http://shivamgupta09.imad.hasura-app.io/counter",true);
-request.send(null);*/
+request.send(null);
