98d732b548e58bd83d5f9c07b800571c4a66d517
shivamgupta09 <shivam.gupta09@gmail.com>
2016-11-25 22:16:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -72,7 +72,7 @@ function createTemplate(data)
            </head>
     <body>
     <video width = "300" height ="300" controls>
-    <source src =""https://www.youtube.com/watch?v=zmR_CYJrz8o" type= "video/FLV">
+    <source src ="http://www.dailymotion.com/video/x2fy49t_spider-man-hd-original-cartoon-classic-animation-for-kids-sub-zero-for-spidey_shortfilms">
         </video>
         <div class ="container">
             
