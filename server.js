192b9db694e7b678953f63936695612e8c7f4665
shivamgupta09 <shivam.gupta09@gmail.com>
2016-11-26 01:11:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -66,7 +66,7 @@ The voice-over is clearly audible. Visuals are crisp and colorful.
 Emotions are pictured perfectly. You can read the story along with kids using the subscripts.
 It is a fascinating fancy story for kids.
 </p>
-<iframe marginheight="0" marginwidth ="0" width ="10px" height ="10px" src="https://drive.google.com/file/d/0Bx_9uS0fhOt_T01ac3didnNJZ2M/preview"></iframe>`
+<iframe marginheight="0" marginwidth ="0" width ="80px" height ="80px" src="https://drive.google.com/file/d/0Bx_9uS0fhOt_T01ac3didnNJZ2M/preview"></iframe>`
     
 },
 'article-two':{
